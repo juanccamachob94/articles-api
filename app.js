ba9f0c439d43f0e14684063f1ece5052ce7959f0
Juan Camilo <juanccamachob94@gmail.com>
2021-06-23 01:17:24 -0500

    slight fix

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(err, req, res, next) {
+app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
