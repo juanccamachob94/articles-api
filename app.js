e6294cbcc93209af44d6b7efaffa02b60b640f1f
Juan Camilo <juanccamachob94@gmail.com>
2021-06-23 11:04:21 -0500

    cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const routes = require('./routes/index');
-const cors = require("cors");
+// const cors = require("cors");
 
 var app = express();
 
@@ -16,12 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+// app.use(cors());
 
 app.use(function(req, res, next) {
-  res.setHeader('Acces-Control-Allow-Origin','*');
-  res.setHeader('Acces-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
-  res.setHeader('Acces-Control-Allow-Headers','Content-Type');
+  res.setHeader('Acces-Control-Allow-Origin', '*');
+  res.setHeader('Acces-Control-Allow-Headers', '*');
+  res.setHeader('Acces-Control-Allow-Methods', '*');
   next();
 });
 
